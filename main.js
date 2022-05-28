67399492e6dd321c5b72c67d9ca954e828f7f05d
aaaram0205 <aaaram0205@gmail.com>
2022-05-29 01:20:19 +0900

    데이터타입 확인 함수/기능 모듈화 후 import, export defualt 예제

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-import _ from 'lodash';
-console.log('hello World');
-console.log(_.camelCase('hello World'));
+import getType from './getType';   //.js는 생략가능하다.
+
+console.log(typeof 'Hello');
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+
+
+
+console.log(getType(undefined));
+console.log(getType(null));
+console.log(getType({}));
+console.log(getType([]));
+console.log(getType(false));
