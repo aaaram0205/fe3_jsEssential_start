8d889a5dca9d1279512d764399c0c29d1c8b95bc
aaaram0205 <aaaram0205@gmail.com>
2022-05-31 00:16:32 +0900

    변수의 유효범위/ 형변환 falsy

--- for.js
+++ for.js
@@ -16,3 +16,30 @@ for (let i = 0; i < 10; i += 1) {
     
     ulEl.appendChild(li);
 }
+
+
+// var: 함수 레벨의 유효범위라 의도지않은 범위에서 변수가 사용될 수있다.
+// let, const: 블록레벨 유효범위
+
+function scope () {
+    
+    if (true) {
+        //const test = 123;
+        var test = 123;
+        
+    }
+    //console.log('scope: ', test);
+    console.log('scope: ', test);
+}
+
+scope ()
+
+
+// 형변환
+// 일치연산자를 써주자. (===, 자료형까지 비교), 동등연산자는 의도치않은 값을 가져옴
+// truthy (참과 같은 값) : true, 'false', 1,2, {}, [], '3.14', -12.....
+//falsy (거짓과 같은 값) : false, ' ', undefined, null, NaN, 0, -0 
+
+if ('false') {
+    console.log(123456789)
+  }
